232bd535cb2674813227a8aeb1d73ffd59c6b25f
Aymane Himame <52378474+aymane-06@users.noreply.github.com>
2024-07-01 16:09:32 +0100

    test hh

---
+++ script.js
@@ -0,0 +1,12 @@
+let NumberOne = 10;
+let NumberTow = 20;
+console.log(NumberOne+""+NumberTow)
+console.log(typeof(NumberOne+""+NumberTow))
+console.log(`${NumberOne}${NumberTow}`)
+console.log(typeof(`${NumberOne}${NumberTow}`))
+console.log(`I'm\ In\n
+\\\\n
+Love\ \\\\ \"\"\"\ \'\'\'
+\n\+\+ With \+\+
+\\"\"\"\\"\"\"
+\"\"JavaScript\"\"\``)
