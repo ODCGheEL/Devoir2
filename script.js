c31ec238e55b880a39e36d42d3eee98ed45b6910
ODCGheEL <ghizodakdak@gmail.com>
2024-07-01 14:54:09 +0100

    v1.0

--- script.js
+++ script.js
@@ -5,7 +5,28 @@ const navbar = document.querySelector(".menubar");
 const toggleNav = () => {
   navbar.classList.toggle("active");
   mobileNav.classList.toggle("hamburger-active");
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 };
 mobileNav.addEventListener("click", () => toggleNav());
+
+// goto top functionality
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 200) {
+    // Show the button when scrolling past 200 pixels
+    document.getElementById("go-to-top").style.display = "block";
+  } else {
+    document.getElementById("go-to-top").style.display = "none";
+  }
+});
+
+document.getElementById("go-to-top").addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
 
 // form validation
